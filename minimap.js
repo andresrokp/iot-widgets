1334edd1a5d28ac174bd58790ef309f4d53473d3
Andres Roca P <afrokap@gmail.com>
2022-10-02 12:56:31 -0500

    adding more reference points

--- minimap.js
+++ minimap.js
@@ -6,32 +6,36 @@
 function positionConversion(origXPos, origYPos){
     // origin (TopLeft), TopRight, BottomLeft, BottomRight
 
-    let originCorner = { lat : 4.696451, lon : -74.140944};
+    let originCorner = { lat : 4.696451, lon : -74.141394} // { lat : 4.696451, lon : -74.140944} //{ lat : 4.691746460628777, lon : -74.14087057113649};
     let trCorner = { lat : 4.6895389, lon : -74.1314638888888};
     let blCorner = { lat : 4.6924722, lon : -74.14475};
     let brCorner = { lat : 4.6854, lon : -74.1346888888889};
-    let rotationDeg = 30 // 52.390409;
+    let rotationDeg = 31.26 // 52.390409;
 
     // let point = { x : origXPos, y : origYPos};
     let echoCorners = {
-        '15-14-inf':{ x : -74.135718, y: 4.689274},
-        '15-14-sup':{ x : -74.135314, y: 4.689811},
-        '14-13-inf':{ x : -74.136262, y: 4.689672},
-        '14-13-sup':{ x : -74.135851, y: 4.690206},
-        '13-12-inf':{ x : -74.13676142692567, y: 4.690043616511041},
-        '13-12-sup':{ x : -74.13625717163087, y: 4.690714596400146},
-        '12-11-inf':{ x : -74.13728713989259, y: 4.690439254133196},
-        '12-11-sup':{ x : -74.13679361343385, y: 4.691118253313656},
-        'linea-roja-11-10':{x:-74.137809,y:4.690847} ,
-        'linea-roja-15-14':{x:-74.1357207298279,y:4.689255013825293} 
+        // '15-14-inf':{ x : -74.135718, y: 4.689274},
+        // '15-14-sup':{ x : -74.135314, y: 4.689811},
+        // '14-13-inf':{ x : -74.136262, y: 4.689672},
+        // '14-13-sup':{ x : -74.135851, y: 4.690206},
+        // '13-12-inf':{ x : -74.13676142692567, y: 4.690043616511041},
+        // '13-12-sup':{ x : -74.13625717163087, y: 4.690714596400146},
+        // '12-11-inf':{ x : -74.13728713989259, y: 4.690439254133196},
+        // '12-11-sup':{ x : -74.13679361343385, y: 4.691118253313656},
+        'linea-roja-11-10':{x:-74.13780212402345,y:4.69083221830631},
+        'linea-roja-15-14':{x:-74.1357207298279,y:4.689255013825293},
+        'esquina-extrema-inf':{x:-74.13452982902528,y:4.685881388218541},
+        'esquina-extrema-izq':{x:-74.14269447326662,y:4.693879688058168},
+        'esquina-extrema-inf-2':{x:-74.13561880588533,y:4.6866940538791635},
+        'esquina-calle-cuadros':{x:-74.13502871990205,y:4.687463946791265}
     }
 
-    let point = { x : origXPos, y : origYPos}; // este es el propio, UncommentToGo
+    // let point = { x : origXPos, y : origYPos}; // este es el propio, UncommentToGo
 
     // prueba de esquinas echos
     let cornersArray = Object.keys(echoCorners);
     let chooseNum = Math.floor(Math.random() * cornersArray.length)
-    // let point = echoCorners[cornersArray[chooseNum]]  // este es dummy, CommentToGo
+    let point = echoCorners[cornersArray[chooseNum]]  // este es dummy, CommentToGo
 
     // traslación al origen
     point.x = point.x - originCorner.lon;
@@ -46,8 +50,8 @@ function positionConversion(origXPos, origYPos){
     point.y = -point.y
 
     // transformaciones de escala
-    point.x = point.x * 88;
-    point.y = point.y * 185;
+    point.x = point.x * 84;
+    point.y = point.y * 285;
 
     return point;
     // return {x : Math.random(), y: Math.random()};
