ea758a4f95756008aadb4dfe7dba74048f034564
Andres Roca P <afrokap@gmail.com>
2022-10-05 22:05:25 -0500

    init vehiclesList.js | cellStyleTemp()

---
+++ vehiclesBulkList.js
@@ -0,0 +1,10 @@
+function cellStyleTemp(value){
+    let style = {};
+
+    let bckColor = "LawnGreen";
+    if (value < 30) bckColor = "LightBlue ";
+    if (value > 60) bckColor = "OrangeRed";
+    style["background-color"] = bckColor
+
+    return style
+}
